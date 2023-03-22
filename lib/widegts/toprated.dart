225355251677f244db52572b0624cf89e00a0d97
Deviletha <devicathira1991@gmail.com>
2023-03-22 13:44:49 +0530

    commit

--- lib/widegts/toprated.dart
+++ lib/widegts/toprated.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/utils/text.dart';
 
+import '../description.dart';
+
 class TopRated extends StatelessWidget {
   final List toprated;
 
@@ -26,7 +28,25 @@ class TopRated extends StatelessWidget {
                 scrollDirection: Axis.horizontal,
                 itemBuilder: (context, index) {
                   return InkWell(
-                    onTap: () {},
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Description(
+                                name: toprated[index]['title'] ?? 'Data not availabele',
+                                bannerurl: toprated[index]['backdrop_path'] != null
+                                    ? 'https://image.tmdb.org/t/p/w500' + toprated[index]['backdrop_path']
+                                    : 'https://thumbs.dreamstime.com/z/error-sign-error-message-white-background-error-sign-error-message-simple-vector-icon-125098995.jpg',
+                                posterurl: toprated[index]['poster_path'] != null
+                                    ? 'https://image.tmdb.org/t/p/w500' + toprated[index]['poster_path']
+                                    : 'https://thumbs.dreamstime.com/z/error-sign-error-message-white-background-error-sign-error-message-simple-vector-icon-125098995.jpg',
+                                description: toprated[index]['overview'] ?? 'Data not availabele',
+                                vote: toprated[index]['vote_average']?.toString() ?? '0.00',
+                                launch_on: toprated[index]['release_date'] ?? 'Data not availabele',
+                              )
+                          )
+                      );
+                    },
                     child: Container(
                       width: 140,
                       child: Column(
